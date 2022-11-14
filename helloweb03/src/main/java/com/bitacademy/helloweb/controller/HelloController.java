4edcca6f642776ae4138cc007db6dc98b79c8faf
Son-Sumin <goldkitty007@gmail.com>
2022-11-14 14:53:52 +0900

    helloweb03 #3

--- helloweb03/src/main/java/com/bitacademy/helloweb/controller/HelloController.java
+++ helloweb03/src/main/java/com/bitacademy/helloweb/controller/HelloController.java
@@ -10,22 +10,29 @@ public class HelloController {
 	public String hello() {
 		return "/WEB-INF/views/hello.jsp";
 	}
+	
+	@RequestMapping("/hello2")  // Long.parselong 하지 않아도 알아서 해줌~
+	public String hello(String name, Long no) {
+		System.out.println("name: " + name + ", no: " + no);
+		return "/WEB-INF/views/hello.jsp";
+	}
 }
 
 
 /* 흔히 실수하는 부분
 
-1. public class HelloController; not import @Controller
+1. HelloController.java -> not import @Controller
 
 2. Dispatcher Servlet 관련 WEB-INF/spring(서블릿 이름)-servlet.xml 만들기
 
-3. spring-servlet.xml; base-package="com.bitacademy.helloweb.controller" 오타
+3. spring-servlet.xml -> base-package="com.bitacademy.helloweb.controller" 오타
 
 
 <<순서>>
 helloweb03/pom.xml; properties, dependencies, build 추가 및 확인
 -> helloweb03/WEB-INF/web.xml; Dispatcher Servlet 설정
 -> helloweb03/WEB-INF/spring-servlet.xml 생성 후 base-package 설정(오타 주의)
+	base-package에서 설정한 파일 이하로 탐색하기 때문에 세부적으로 설정하는 것이 좋음
 -> com.bitacademy.helloweb.controller 패키지 생성, HelloController.java 생성
 -> HelloController.java에서 @Controller, @RequestMapping("/hello") 주의하여 작성
 -> /WEB-INF/views/hello.jsp 생성 ()
