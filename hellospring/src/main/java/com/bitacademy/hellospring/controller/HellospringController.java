f65f42444b7b56d6004e78bc79b21478c097189b
Son-Sumin <goldkitty007@gmail.com>
2022-11-14 16:16:22 +0900

    hellospring

---
+++ hellospring/src/main/java/com/bitacademy/hellospring/controller/HellospringController.java
@@ -0,0 +1,13 @@
+package com.bitacademy.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HellospringController {
+	
+	@RequestMapping("/hi")
+	public String hi() {
+		return "WEB-INF/views/hi.jsp";
+	}
+}
