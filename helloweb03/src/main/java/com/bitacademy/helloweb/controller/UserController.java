7c1529a07e704f5e3f385c6e4055f3fe684c384d
Son-Sumin <goldkitty007@gmail.com>
2022-11-15 11:33:25 +0900

    @RequestMapping & @RequestParam

--- helloweb03/src/main/java/com/bitacademy/helloweb/controller/UserController.java
+++ helloweb03/src/main/java/com/bitacademy/helloweb/controller/UserController.java
@@ -2,25 +2,53 @@ package com.bitacademy.helloweb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /*
  * 추천!
  * @RequestMapping 클래스 + 메소드
+ * 같은 URL, 다른 메소드 
+ * 
+ * 변수이름은 상세히, 파라미터는 보여지는 의미 없게
+ * 
+ * 기본값 세팅, 400으로 가지 않도록!
  */
 @Controller
 @RequestMapping("/user")
 public class UserController {
 	
-	@ResponseBody
-	@RequestMapping("/joinform")
-	public String joinform() {
-		return "UserController.joinform()";
+	@RequestMapping(value="/join", method=RequestMethod.GET)
+	public String join() {
+		return "/WEB-INF/views/join.jsp";  // forward
+	}
+	
+	@RequestMapping(value="/join", method=RequestMethod.POST)
+	public String join(UserVo vo) {
+		System.out.println(vo);
+		return "redirect:/";  // redirect
 	}
 	
 	@ResponseBody
-	@RequestMapping("/join")
-	public String join() {
-		return "UserController.join()";
+	@RequestMapping("/update")
+	public String update(@RequestParam("n") String name) {  // "n"; 변수 이름 같아도 써주기, 다르면 꼭 쓰기
+		/*
+		 * 만일 n이라는 이름의 URL 파라미터가 없는 경우
+		 * 400(Bad Request) 에러가 발생한다.
+		 */
+		return "UserController.update(" + name + ")";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/update2")
+	public String update2(@RequestParam(value="n", required=true, defaultValue="") String name) {
+		return "UserController.update(" + name + ")";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/list")
+	public String list(@RequestParam(value="p", required=true, defaultValue="1") int pageNo) { // int여도 값 지정은 "1", int로 나옴
+		return "UserController.list(" + pageNo + ")";
 	}
 }
