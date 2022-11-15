ad9725b0efb848abbedcee02418d32f4e5c76b32
Son-Sumin <goldkitty007@gmail.com>
2022-11-15 14:52:56 +0900

    emaillist03

--- emaillist03/src/main/java/com/bitacademy/emaillist/controller/EmaillistController.java
+++ emaillist03/src/main/java/com/bitacademy/emaillist/controller/EmaillistController.java
@@ -1,20 +1,37 @@
 package com.bitacademy.emaillist.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.bitacademy.emaillist.repository.EmaillistRepository;
+import com.bitacdemy.emaillist.vo.EmaillistVo;
 
 @Controller
 public class EmaillistController {
 	@Autowired  // Root EmailRepo -> Web EmailRepo 자동 DI!!
 	private EmaillistRepository emaillistRepository;
 	
-	@ResponseBody  // test 위해 나에게 받기
+	// @ResponseBody  // test 위해 나에게 받기
 	@RequestMapping("")
-	public String index() {
-		return "index!!";
+	public String index(Model model) {
+		List<EmaillistVo> list = emaillistRepository.findAll();
+		model.addAttribute("list", list);
+		return "/WEB-INF/views/index.jsp";
+	}
+	
+	@RequestMapping(value="/add", method=RequestMethod.GET)
+	public String add() {
+		return "/WEB-INF/views/add.jsp";
+	}
+	
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public String add(EmaillistVo vo) {
+		emaillistRepository.insert(vo);
+		return "redirect:/";
 	}
 }
